de6ed46ea363e5720a7e96cfeb437a8c94a4c6b5
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-08-31 23:45:25 -0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+jQuery(function ($) {
+    $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
+    //add fix header when scroll down and remove when back up
+        if (scrollTop > $('header').height()) {
+          $('header').addClass('scrollNav');
+    
+    
+        }
+    
+        else {
+          $('header').removeClass('scrollNav');
+        }
+    
+      });
+
+
+
+});
+
