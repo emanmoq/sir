2e08d6fdd45a235cee5d4a408f6ae2bfcc4e1201
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-09-01 03:41:41 -0700

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,36 @@
 jQuery(function ($) {
     $(window).scroll(function () {
         scrollTop = $(window).scrollTop();
-    //add fix header when scroll down and remove when back up
+        //add fix header when scroll down and remove when back up
         if (scrollTop > $('header').height()) {
-          $('header').addClass('scrollNav');
-    
-    
-        }
-    
-        else {
-          $('header').removeClass('scrollNav');
-        }
-    
-      });
+            $('header').addClass('scrollNav');
 
+
+        }
+
+        else {
+            $('header').removeClass('scrollNav');
+        }
+
+    });
+    $("#createSite").on("shown.bs.modal", function() {
+
+    'use strict';
+    var pgb = $('#progressBar');
+    var i = 0;
+    pgb.ariaProgressbar({
+      progressClass: 'progress progress_streamtext',
+      maxVal: 100
+    });
+
+    var int = setInterval(function() {
+      if (i < 100) {
+        i = i + 2;
+        pgb.ariaProgressbar('update', i);
+      } else {
+        clearInterval(int);
+      }
+    }, 400);
+});
 });
 
